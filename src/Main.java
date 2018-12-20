4bdd33c5952474843f53ef0c838ce4bfa4fefb31
Mikhail Materukhin <9226343456@mail.ru>
2018-12-20 16:08:30 +0500

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.gameMenu();
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        primaryStage.setTitle("Крестики-нолики");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {launch(args); }
 }
